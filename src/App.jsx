3908af99e5efea06c77411f3b45b06de4f4db076
Ebanx3 <esteban.dos.santos@gmail.com>
2024-05-28 23:39:15 -0300

    primer commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import "./App.css";
+import Navbar from "./componentes/navbar/Navbar";
+
+function App() {
+  
+
+  return (
+    <><Navbar />
+    <div>
+      <p className="texto">
+        Hola como va?
+      </p>
+    </div></>
+  )
+}
+
+export default App
