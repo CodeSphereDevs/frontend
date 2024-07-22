6c552859d475b07bd08b969c0e9805ae08589377
Jackson Quintero <75138506+codigoarchivo@users.noreply.github.com>
2024-07-22 09:45:38 -0400

    creado metodo createProject

---
+++ src/api/createProject.js
@@ -0,0 +1,18 @@
+const CREATEPOST_ENDPOINT = `${import.meta.env.VITE_SERVER_URI}/api/projects/`;
+
+export const createProject= async (title, content) => {
+    try{
+        const response = await fetch(CREATEPOST_ENDPOINT, {
+            method: "POST",
+            headers: {
+                "Content-Type":"application/json",
+            },
+            credentials:"include",
+            body: JSON.stringify({title, content})
+        })
+        return await response.json();
+    }catch(error){
+        console.log(error);
+        return null;
+    }
+}
