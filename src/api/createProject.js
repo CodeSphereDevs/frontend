53a40f7647d83f0172495933f58401c49a7a791b
Jackson Quintero <75138506+codigoarchivo@users.noreply.github.com>
2024-08-03 10:08:26 -0400

    refactor actions createProject

--- src/api/createProject.js
+++ src/api/createProject.js
@@ -1,18 +1,29 @@
 const CREATEPOST_ENDPOINT = `${import.meta.env.VITE_SERVER_URI}/api/projects/`;
 
-export const createProject= async (title, content) => {
-    try{
-        const response = await fetch(CREATEPOST_ENDPOINT, {
-            method: "POST",
-            headers: {
-                "Content-Type":"application/json",
-            },
-            credentials:"include",
-            body: JSON.stringify({title, content})
-        })
-        return await response.json();
-    }catch(error){
-        console.log(error);
-        return null;
+export const createProject = async ({
+  projectName,
+  details,
+  technologies,
+  numMembers,
+}) => {
+  try {
+    const response = await fetch(CREATEPOST_ENDPOINT, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      credentials: "include",
+      body: JSON.stringify({ projectName, details, technologies, numMembers }),
+    });
+
+    if (!response.ok) {
+      throw new Error(`Error: ${response.status} ${response.statusText}`);
     }
-}
+
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("Failed to create project:", error);
+    return null;
+  }
+};
